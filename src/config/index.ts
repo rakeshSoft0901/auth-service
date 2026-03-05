dbc7ca2ccea11d561125a4817a7b99e7a9acbae6
rakeshsingh0901 <your.email@example.com>
2026-03-05 18:53:03 +0530

    Connect database

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,10 +1,18 @@
 import dotenv from 'dotenv'
+import path from 'path'
 
-dotenv.config()
+dotenv.config({
+  path: path.join(__dirname, `../../.env.${process.env.NODE_ENV}`),
+})
 
 const Config = {
   PORT: process.env.PORT,
   NODE_ENV: process.env.NODE_ENV,
+  DATABASE_NAME: process.env.DATABASE_NAME,
+  DB_USER: process.env.DB_USER,
+  DB_PASSWORD: process.env.DB_PASSWORD,
+  DB_PORT: process.env.DB_PORT,
+  DB_HOST: process.env.DB_HOST,
 }
 
 export default Config
---
+++ src/data-source.ts
@@ -0,0 +1,19 @@
+import 'reflect-metadata'
+import { DataSource } from 'typeorm'
+import { User } from './entity/User'
+import Config from './config'
+
+export const AppDataSource = new DataSource({
+  type: 'postgres',
+  host: Config.DB_HOST || 'localhost',
+  port: parseInt(Config.DB_PORT || '5432'),
+  username: Config.DB_USER || 'root',
+  password: Config.DB_PASSWORD || 'root',
+  database: Config.DATABASE_NAME || 'auth_service',
+  // synchronize: true will automatically create the database schema on application startup based on your entities.
+  synchronize: Config.NODE_ENV === 'development' || Config.NODE_ENV === 'test',
+  logging: false,
+  entities: [User],
+  migrations: [],
+  subscribers: [],
+})
