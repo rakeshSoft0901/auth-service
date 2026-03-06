3f765145adf0d257b718b6ddfeeb58b32d938bcd
rakeshsingh0901 <your.email@example.com>
2026-03-06 16:11:05 +0530

    Add schema level validators

--- src/validators/register-validator.ts
+++ src/validators/register-validator.ts
@@ -1,3 +1,10 @@
-import { body } from 'express-validator'
+import { checkSchema } from 'express-validator'
 
-export default [body('email').isEmail().withMessage('Invalid email address')]
+// export default [body('email').isEmail().withMessage('Invalid email address')]
+
+export default checkSchema({
+  email: {
+    errorMessage: 'Invalid email address',
+    isEmail: true,
+  },
+})
